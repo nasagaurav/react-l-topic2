0eb7e345b7c06d5092420dd3f07cbb3aeb702e5e
nasagaurav <nasa.gauravnasa@gmail.com>
2022-07-19 07:34:55 +0530

    added function skeleton

--- src/App.js
+++ src/App.js
@@ -2,14 +2,22 @@ import React from "react";
 import "./style.css";
 
 export default function App() {
+
+  const p1=()=>{}
+  const p2=()=>{}
+  const p3=()=>{}
+  const p4=()=>{}
+  const p5=()=>{}
+  const p6=()=>{}
+
   return (
     <div>
-      <button>click to change title 1</button>
-      <button>click to change title 2</button>
-      <button>click to change title 3</button>
-      <button>click to change title 4</button>
-      <button>click to change title 5</button>
-      <button>click to change title 6</button>
+      <button onClick={p1}>click to change title 1</button>
+      <button onClick={p2}>click to change title 2</button>
+      <button onClick={p3}>click to change title 3</button>
+      <button onClick={p4}>click to change title 4</button>
+      <button onClick={p5}>click to change title 5</button>
+      <button onClick={p6}>click to change title 6</button>
     </div>
   );
 }
