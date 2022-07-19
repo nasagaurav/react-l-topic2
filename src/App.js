c2120b9603fec0d086782f107a930011ff52e6cf
nasagaurav <nasa.gauravnasa@gmail.com>
2022-07-19 08:00:06 +0530

    added controlled input via state

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
 
 export default function App() {
+  const [topic, settopic] = React.useState('html');
 
+  const handleChange = (e) => {
+    settopic(e.target.value);
+  };
   return (
     <div>
-      <input /> 
+      <input value={topic} onChange={handleChange} />
       <button>add topic </button>
       <ol>
         <li>topic name is </li>
-      </ol>     
+      </ol>
     </div>
   );
 }
