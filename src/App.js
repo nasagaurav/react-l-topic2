fa29512b595c48911890a32f4d7b664979b99217
nasagaurav <nasa.gauravnasa@gmail.com>
2022-07-19 07:34:07 +0530

    example2 added html

--- src/App.js
+++ src/App.js
@@ -4,7 +4,12 @@ import "./style.css";
 export default function App() {
   return (
     <div>
-     
+      <button>click to change title 1</button>
+      <button>click to change title 2</button>
+      <button>click to change title 3</button>
+      <button>click to change title 4</button>
+      <button>click to change title 5</button>
+      <button>click to change title 6</button>
     </div>
   );
 }
