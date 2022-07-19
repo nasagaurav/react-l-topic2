d7a6785d43a2772685fb12f60017592f0b6c7299
nasagaurav <nasa.gauravnasa@gmail.com>
2022-07-19 07:17:56 +0530

    added html for useEffect and usestate

--- src/App.js
+++ src/App.js
@@ -2,9 +2,13 @@ import React from "react";
 import "./style.css";
 
 export default function App() {
+  
+
   return (
     <div>
-     
+      <h1>counter ? </h1>
+      <h2>changes made are ? </h2>
+      <button>+</button>
     </div>
   );
 }
