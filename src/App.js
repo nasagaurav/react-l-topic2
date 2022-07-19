7dab123fb858457241d76eab377922677b8760b4
nasagaurav <nasa.gauravnasa@gmail.com>
2022-07-19 07:58:29 +0530

    example4 added html

--- src/App.js
+++ src/App.js
@@ -2,9 +2,14 @@ import React from "react";
 import "./style.css";
 
 export default function App() {
+
   return (
     <div>
-     
+      <input /> 
+      <button>add topic </button>
+      <ol>
+        <li>topic name is </li>
+      </ol>     
     </div>
   );
 }
