15a178e866d29d14466d7ffdec193110375ce25a
nasagaurav <nasa.gauravnasa@gmail.com>
2022-07-19 07:20:40 +0530

    added + and - functionality for button

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
 
 export default function App() {
-  const [x,setx]=React.useState(0)
-  const [y,sety]=React.useState(0)
+  const [x, setx] = React.useState(0);
+  const [y, sety] = React.useState(0);
 
-  const p1=()=>{}
+  const p1 = () => {
+    setx(x + 1);
+  };
+  const p2 = () => {
+    setx(x - 1);
+  };
 
   return (
     <div>
       <h1>counter {x} </h1>
       <h2>changes made are {y} </h2>
       <button onClick={p1}>+</button>
+      <button onClick={p2}>-</button>
     </div>
   );
 }
