c7268a9a58aa47c7e2865cbe275fc24841dba80c
nasagaurav <nasa.gauravnasa@gmail.com>
2022-07-19 07:39:37 +0530

    example3 counter example again

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
 
 export default function App() {
+  const [x, setx] = React.useState(0);
+
+  const p1 = () => {
+    setx(x + 1);
+  };
+  const p2 = () => {
+    setx(x - 1);
+  };
+  const p3 = () => {
+    setx(0);
+  };
+
   return (
     <div>
-     
+      <button onClick={p1}>+</button>
+      <button onClick={p2}>-</button>
+      <button onClick={p3}>0</button>
     </div>
   );
 }
