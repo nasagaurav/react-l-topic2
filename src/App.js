5f9e5afbc0ca8d01e1779d8bd5d496739113b95c
nasagaurav <nasa.gauravnasa@gmail.com>
2022-07-19 07:18:55 +0530

    added state and function

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from "react";
 import "./style.css";
 
 export default function App() {
-  
+  const [x,setx]=React.useState(0)
+  const [y,sety]=React.useState(0)
+
+  const p1=()=>{}
 
   return (
     <div>
-      <h1>counter ? </h1>
-      <h2>changes made are ? </h2>
-      <button>+</button>
+      <h1>counter {x} </h1>
+      <h2>changes made are {y} </h2>
+      <button onClick={p1}>+</button>
     </div>
   );
 }
